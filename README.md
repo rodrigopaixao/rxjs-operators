be33604ecd1ab44d88fd1f026eebf12ccdd81a4b
rodrigopaixao <rodrigopaixao@gmail.com>
2020-02-11 23:23:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rp-rxjs-operators
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rp-rxjs-operators)
---
+++ index.ts
@@ -0,0 +1,39 @@
+import { of, interval, from, timer } from 'rxjs'; 
+import { map, mergeMap, switchMap } from 'rxjs/operators';
+
+// map operator example
+
+console.log('map ----------------------------');
+
+const mapSource = of('Original String').pipe(
+  map(string => `Modified String!`)
+);
+
+mapSource.subscribe(x => console.log(x));
+
+// switchMap operator example
+
+console.log('switchMap ----------------------------');
+
+const switched = of(1, 2, 3)
+  .pipe(
+    switchMap((x: number) => of(x, x * 2)
+      .pipe(
+        map(param => `Value ${param}`)
+      )
+    )
+);
+
+switched.subscribe(x => console.log(x));
+
+// mergeMap operator
+
+console.log('mergeMap ----------------------------');
+
+const letters = of('a', 'b', 'c');
+
+const result = letters.pipe(
+  mergeMap(x => from([1,2,3]).pipe(map(i => x+i))),
+);
+
+result.subscribe(x => console.log(x));
